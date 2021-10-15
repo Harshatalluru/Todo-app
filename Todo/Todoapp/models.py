6ac4d42cd822af509f72071dc8055c6632db62b9
harishtalluru <harshatalluru20@gmail.com>
2021-10-15 20:57:36 +0530

    first commit

--- Todo/Todoapp/models.py
+++ Todo/Todoapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 
 class Todo(models.Model):
     task = models.CharField(max_length=200)
+    Create_at = models.DateTimeField(default=datetime.now,blank=True)
 
     def __str__(self):
         return self.task
